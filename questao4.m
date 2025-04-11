0adab9235c687b5f8bb7d55aa41d6c52bfb97411
Jamerson Lima / <101479828+jamisu337@users.noreply.github.com>
2025-04-11 11:56:38 -0300

    Update questao4.m

--- questao4.m
+++ questao4.m
@@ -1,12 +1,18 @@
-altura = input('Digite sua altura (em metros): ');
+altura = input('Digite sua altura (em metros): ', 's');
 sexo = input('Digite seu sexo (M para masculino, F para feminino): ', 's');
 
-if upper(sexo) == 'M'
-    peso = 72.7 * altura - 58;
-    fprintf('Peso ideal: %.2f kg\n', peso);
-elseif upper(sexo) == 'F'
-    peso = 62.1 * altura - 44.7;
-    fprintf('Peso ideal: %.2f kg\n', peso);
+altura = str2double(altura);
+
+if isnan(altura)
+    disp('Erro: a entrada não é um número válido.');
+elseif upper(sexo) == 'M' || upper(sexo) == 'F'
+    if upper(sexo) == 'M'
+        peso = 72.7 * altura - 58;
+        fprintf('Peso ideal: %.2f kg\n', peso);
+    elseif upper(sexo) == 'F'
+        peso = 62.1 * altura - 44.7;
+        fprintf('Peso ideal: %.2f kg\n', peso);
+    end
 else
     disp('Sexo inválido. Use M ou F.');
 end
