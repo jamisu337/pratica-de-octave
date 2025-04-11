b235b2118b532065c08f566daf9a2c69e596b8fe
Jamerson Lima / <101479828+jamisu337@users.noreply.github.com>
2025-04-11 11:17:17 -0300

    Update questao3.m

--- questao3.m
+++ questao3.m
@@ -1,10 +1,16 @@
-nota1 = input('Digite a primeira nota (0.0 a 10.0): ');
-nota2 = input('Digite a segunda nota (0.0 a 10.0): ');
+nota1 = input('Digite a primeira nota (0.0 a 10.0): ', 's');
+nota2 = input('Digite a segunda nota (0.0 a 10.0): ', 's');
 
-if (nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10)
-    disp('Nota(s) inválida(s). O programa será encerrado.');
+nota1 = str2double(nota1);
+nota2 = str2double(nota2);
+
+if isnan(num1) || isnan(num2)
+    disp('Erro: uma ou ambas as entradas não são números válidos.');
 else
-    media = (nota1 + nota2) / 2;
-    fprintf('A média das notas é: %.2f\n', media);
+    if (nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10)
+        disp('Nota(s) inválida(s). programa encerrado.');
+    else
+        media = (nota1 + nota2) / 2;
+        fprintf('A média das notas é: %.2f\n', media);
+    end
 end
-
