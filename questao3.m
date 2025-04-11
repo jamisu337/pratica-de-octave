30137abac65bcbdf51fd4e9f9c952ae2feb0dc03
Jamerson Lima / <101479828+jamisu337@users.noreply.github.com>
2025-04-11 11:57:47 -0300

    Update questao3.m

--- questao3.m
+++ questao3.m
@@ -4,7 +4,7 @@ nota2 = input('Digite a segunda nota (0.0 a 10.0): ', 's');
 nota1 = str2double(nota1);
 nota2 = str2double(nota2);
 
-if isnan(num1) || isnan(num2)
+if isnan(nota1) || isnan(nota2)
     disp('Erro: uma ou ambas as entradas não são números válidos.');
 else
     if (nota1 < 0 || nota1 > 10 || nota2 < 0 || nota2 > 10)
