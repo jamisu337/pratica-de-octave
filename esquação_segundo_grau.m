4926e67c38a04c367c7de6824d3eb4697b20f7af
Jamerson Lima / <101479828+jamisu337@users.noreply.github.com>
2025-04-14 21:17:38 -0300

    Update esquação_segundo_grau.m

--- esquação_segundo_grau.m
+++ esquação_segundo_grau.m
@@ -13,7 +13,7 @@ a = str2double(a);
 b = str2double(b);
 c = str2double(c);
 
-delta = (b*b)-(4*a*c);
+delta = (b*b)-(4*a*c)
 
 if isnan(a) || isnan(b) || isnan(c)
     disp('Erro: uma ou ambas as entradas não são números válidos.');
@@ -24,12 +24,12 @@ else
         x = (-b)/2*a;
         fprintf('o valor de x é: %.2f\n', x);
     elseif delta > 0
-        x1 = (-b+delta)/(2*a);
-        x2 = (-b-delta)/(2*a);
+        x1 = (-b+sqrt(delta))/(2*a);
+        x2 = (-b-sqrt(delta))/(2*a);
         fprintf('os valores de x sao: \n x1: %.2f\n x2: %.2f\n', x1, x2);
     end
 end
 
-verificador = input('para resolver novamente digite 1: ');
+verificador = input('digite 1 para resolver novamente, digite 0 para encerrar o programa: ');
 
 end
